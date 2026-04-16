3e557af4ccd3b599ad97301aedf3d1b4538ebc64
Rhys Horner <18751996@student.westernsydney.edu.au>
2026-04-16 22:01:37 +1000

    almost there

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,15 @@
 #include "system/loop.h"
 #include "utils/log.h"
 #include "control/control.h"
+#include <PS4Controller.h>
+#include "esp_bt_device.h"
 
 void setup() {
     Serial.begin(115200);
     delay(1000);
-
+    
     log(INFO, "System starting");
-
+    //MAC: 30:ae:a4:ff:16:10
     initControl();
     initLoop();
 }
