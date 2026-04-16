e7c6701670b18147c978d2fcfc34b1eb9a9deefa
Rhys18751996 <18751996@student.westernsydney.edu.au>
2026-04-17 07:49:58 +1000

    printing all the mac addresses at startup

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,17 @@
 #include "system/loop.h"
 #include "utils/log.h"
 #include "control/control.h"
+#include "system/mac.h"
 
 void setup() {
     Serial.begin(115200);
     delay(1000);
     
     log(INFO, "System starting");
+
+    printMacAddresses();
     //MAC: 30:ae:a4:ff:16:10
+
     initControl();
     initLoop();
 }
